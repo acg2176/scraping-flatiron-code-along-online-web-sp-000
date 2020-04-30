d32c8847c8e7968795bc7bf328a84cbe2e2e7746
Anna Guerrero <acg2176@columbia.edu>
2020-04-30 17:16:01 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -7,31 +7,6 @@ class Course
     @@all << self
   end
 
-  def title=(title)
-    @title = title
-
-  end
-
-  def title
-    @title
-  end
-
-  def schedule=(schedule)
-    @schedule = schedule
-  end
-
-  def schedule
-    @schedule
-  end
-
-  def description=(description)
-    @description = description
-  end
-
-  def description
-    @description
-  end
-
   def self.reset_all
     @@all.clear
   end
